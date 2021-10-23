5775ceb81d14a5bcdd3af5f749a347df6465fe03
lopez824 <35384909+lopez824@users.noreply.github.com>
2021-10-23 13:42:44 -0600

    Updated message

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ const IndexPage = () => (
     <Seo title="Welcome!" />
     <h2 className="message">Software Developer</h2>
     <p className="message">I am a graduate student at the University of Utah pursuing a Masters Degree in Entertainment Arts and Engineering.</p>
-    <p className="message">My goal is to work in the game industry as a gameplay or UI programmer. Below is a list of applications I have worked on and wish to share with you.</p>
+    <p className="message">My goal is to work in the game industry as a gameplay, UI, or VR programmer. Below is a list of applications I have worked on and wish to share with you.</p>
     <p className="message"></p>
     <Container>
       <Row>
