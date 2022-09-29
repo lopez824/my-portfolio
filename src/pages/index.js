5c3b07d67c1b37cfbf35e445968ba273f71bd9c7
lopez824 <lionelslopez14@gmail.com>
2022-09-29 16:09:19 -0600

    Updated Card HTML and CSS
    For on hover events and links

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ const IndexPage = () => (
     <Container>
       <Row>
         <Col md={6}>
-          <Card>
+          <Card title="Only a gif is available for this project!">
             <Image src={aou} className="card-img" alt="All of Us" fluid />
             <Card.Body>
               <Card.Title><h2>All of Us</h2></Card.Title>
@@ -44,79 +44,89 @@ const IndexPage = () => (
           </Card>
         </Col>
         <Col md={6}>
+          <a href="https://play.google.com/store/apps/details?id=com.aha_jump.android.communicator" rel="noopener noreferrer" target="_blank" id="card-link">
           <Card>
             <Image src={aha} className="card-img" alt="Kids Heart Challenge" fluid />
             <Card.Body>
               <Card.Title><h2>Kids Heart Challenge</h2></Card.Title>
               <Card.Text>
-                A <a href="https://play.google.com/store/apps/details?id=com.aha_jump.android.communicator" rel="noopener">mobile app</a> for fundraising with the KHC program.
+                A mobile app for fundraising with the KHC program.
               </Card.Text>
               <Card.Text>
               Core responsiblites included implementing new features, fixing bugs, and updating UI.
               </Card.Text>
             </Card.Body>
           </Card>
+          </a>
         </Col>
       </Row>
       <Row>
       <Col md={6}>
+        <a href="https://youtu.be/huajQCQFS5k" rel="noopener noreferrer" target="_blank" id="card-link">
           <Card>
             <Image src={jc} className="card-img" alt="James Castle" fluid />
             <Card.Body>
               <Card.Title><h2>James Castle</h2></Card.Title>
               <Card.Text>
-                A <a href="https://youtu.be/huajQCQFS5k" rel="noopener">VR simulation</a> of Idaho's very own James Castle's studio.
+                A VR simulation of Idaho's very own James Castle's studio.
               </Card.Text>
               <Card.Text>
               Built application for the Oculus Quest, created 3D models, and programmed VR interactions.
               </Card.Text>
             </Card.Body>
           </Card>
+          </a>
         </Col>
         <Col md={6}>
+          <a href="https://play.google.com/store/apps/details?id=com.gimm.NeuralNetwork" rel="noopener noreferrer" target="_blank" id="card-link">
           <Card>
             <Image src={jt} className="card-img" alt="Jacques Tale" fluid />
             <Card.Body>
               <Card.Title><h2>Jacques' Tale</h2></Card.Title>
               <Card.Text>
-                A <a href="https://play.google.com/store/apps/details?id=com.gimm.NeuralNetwork" rel="noopener">mobile game</a> for children inspired by neurons.
+                A mobile game for children inspired by neurons.
               </Card.Text>
               <Card.Text>
               Programmed level system and touch interactions. Created, designed, and programmed UI.
               </Card.Text>
             </Card.Body>
           </Card>
+          </a>
         </Col>
       </Row>
       <Row>
       <Col md={6}>
+        <a href="https://heistnheat.netlify.app/" rel="noopener noreferrer" target="_blank" id="card-link">
           <Card>
             <Image src={hnh} className="card-img" alt="Heist N Heat" fluid />
             <Card.Body>
               <Card.Title><h2>Heist N' Heat</h2></Card.Title>
               <Card.Text>
-                A <a href="https://heistnheat.netlify.app/" rel="noopener">web game</a> iteration of the arcade classic, Rally-X.
+                A web game iteration of the arcade classic, Rally-X.
               </Card.Text>
               <Card.Text>Programmed most of the game. Also hosted the game on Netlify using a Webpack configured for Phaser 3.</Card.Text>
             </Card.Body>
           </Card>
+          </a>
         </Col>
         <Col md={6}>
+        <a href="https://vgmusic.herokuapp.com/" rel="noopener noreferrer" target="_blank" id="card-link">
           <Card>
             <Image src={vg} className="card-img" alt="vgMusic" fluid />
             <Card.Body>
               <Card.Title><h2>vgMusic</h2></Card.Title>
               <Card.Text>
-                My <a href="https://vgmusic.herokuapp.com/" rel="noopener">first website</a> that functions as a music suggestor.
+                My first website that functions as a music suggestor.
               </Card.Text>
               <Card.Text>Built it myself! One cool feature is using AJAX to access a database and embed Spotify Widgets.</Card.Text>
             </Card.Body>
           </Card>
+          </a>
         </Col>
       </Row>
       <Row>
         <Col md={6}>
-          <Card>
+          <Card title="Only a gif is available for this project!">
             <Image src={dol} className="card-img" alt="Disciple of the Land" fluid />
             <Card.Body>
               <Card.Title><h2>Disciple of the Land</h2></Card.Title>
@@ -128,35 +138,38 @@ const IndexPage = () => (
           </Card>
         </Col>
         <Col md={6}>
+          <a href="https://www.youtube.com/watch?v=KLBXxRFKpR8" rel="noopener noreferrer" target="_blank" id="card-link">
           <Card>
             <Image src={gat} className="card-img" alt="Gatling Gun Experience" fluid />
             <Card.Body>
               <Card.Title><h2>Gatling Gun Experience</h2></Card.Title>
               <Card.Text>
-                An <a href="https://www.youtube.com/watch?v=KLBXxRFKpR8" rel="noopener">educational VR game</a> for the Old Idaho Penitentiary.
+                An educational VR game for the Old Idaho Penitentiary.
               </Card.Text>
               <Card.Text>
               Programmed the audio playback system and UI. Excluding textures, designed the UI.
               </Card.Text>
             </Card.Body>
           </Card>
+          </a>
         </Col>
       </Row>
       <Row>
       <Col md={6}>
+        <a href="https://www.youtube.com/watch?v=9-KZYXeqjkM" rel="noopener noreferrer" target="_blank" id="card-link">
       <Card>
             <Image src={demo} className="card-img" alt="Fun Times during Undergrad" fluid />
             <Card.Body>
               <Card.Title><h2>Undergraduate Demo Reel</h2></Card.Title>
               <Card.Text>
-                A <a href="https://www.youtube.com/watch?v=9-KZYXeqjkM" rel="noopener">video</a> showing fun times creating things during my four years at Boise State University.
+                A video showing fun times creating things during my four years at Boise State University.
               </Card.Text>
             </Card.Body>
           </Card>
+          </a>
       </Col>
       </Row>
     </Container>
   </Layout>
 )
-
 export default IndexPage
